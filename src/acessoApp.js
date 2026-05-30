06c6ebfd63a942ae89220a093410a4b617a305b0
danielColona <danielcolona@gmail.com>
2026-05-29 21:25:24 -0300

    Implementação 01

---
+++ src/acessoApp.js
@@ -0,0 +1,40 @@
+// Preciso de uma função capaz de receber duas entradas: Login e Senha. 
+// Ela deverá navegar em um lista pre-existente, com 3 usuários 
+// e então retornar o texto “Logado com sucesso.” 
+// caso exista um usuário com login e senha iguais aos informados.
+// Caso não exista um usuário com login e senha informados, 
+// uma mensagem dizendo “Usuário não encontrado.” deverá ser exibida.
+
+
+
+const usuarios = [{
+  nome: 'Daniel Colona',
+  login: 'danielcolona',
+  senha: 1234
+},
+{
+  nome: 'Ariane Nascimento',
+  login: 'arianenascimento',
+  senha: 4321
+},
+{
+  nome: 'IsaacNascimento',
+  login: 'isaacnascimento',
+  senha: 1324
+}]
+
+
+export function autenticarAcesso(login, senha) {
+    
+    let resultado = 'Usuário não encontrado.';
+    
+    usuarios.forEach(function(usuario){
+        if(usuario.login == login && usuario.senha == senha){  
+            resultado = 'Logado com Sucesso';
+        }
+    });
+    
+    return resultado;
+}
+
+console.log(autenticarAcesso('arianenascimento', 4321));
